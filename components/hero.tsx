6ce9dfd029c74690c635f711edd67f227e81574f
Juan Bernal <mesieou@gmail.com>
2025-04-28 12:01:01 +1000

    changed the main title of the landing page

--- components/hero.tsx
+++ components/hero.tsx
@@ -8,7 +8,7 @@ export default function Header() {
       </div>
       <h1 className="sr-only">Supabase and Next.js Starter Template</h1>
       <p className="text-3xl lg:text-4xl !leading-tight mx-auto max-w-xl text-center">
-        We help small business to manage their bookings and calendars{" "}
+        We help mobile business to manage their bookings and calendars{" "}
       </p>
       <div className="w-full p-[1px] bg-gradient-to-r from-transparent via-foreground/10 to-transparent my-8" />
     </div>
