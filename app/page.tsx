3f2639761b619235a98acc37b73e1e76f77156ba
Daniel Bernal <danielbernal8@gmail.com>
2025-04-27 21:57:11 -0500

    No me dejen solo

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Contador from '../components/Contador';
 import Hero from "@/components/hero";
 import ConnectSupabaseSteps from "@/components/tutorial/connect-supabase-steps";
 import SignUpUserSteps from "@/components/tutorial/sign-up-user-steps";
@@ -10,6 +11,7 @@ export default async function Home() {
       <main className="flex-1 flex flex-col gap-6 px-4">
         <h2 className="font-medium text-xl mb-4">Next steps</h2>
         {hasEnvVars ? <SignUpUserSteps /> : <ConnectSupabaseSteps />}
+        <Contador />
       </main>
     </>
   );
