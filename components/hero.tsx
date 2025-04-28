0495aa1554fe0b9de7bac2d3694592c9e386c83f
Juan Bernal <mesieou@gmail.com>
2025-04-28 22:40:35 +1000

    added the waitlist

--- components/hero.tsx
+++ components/hero.tsx
@@ -2,6 +2,7 @@ import NextLogo from "./next-logo";
 import SupabaseLogo from "./supabase-logo";
 import Video from 'next-video';
 import botDemo from '/videos/bot-demo.mp4';
+import JoinWaitlist  from "@/components/waitlist-form";
 
 export default function Header() {
   return (
@@ -15,10 +16,14 @@ export default function Header() {
             muted
           />
         </div>  
-
-        <p className="text-3xl lg:text-4xl !leading-tight mx-auto max-w-xl text-center">
-          We help mobile business to manage their bookings and calendars with smart agents{" "}
-        </p>
+        <div className="flex flex-col justify-evenly">
+          <p className="text-3xl lg:text-4xl !leading-tight mx-auto max-w-xl text-center">
+            We help mobile business to manage their bookings and calendars with smart agents{" "}
+          </p>
+          <div className="mt-32">
+            <JoinWaitlist />  
+          </div>
+        </div>
       </div>
       <div className="w-full p-[1px] bg-gradient-to-r from-transparent via-foreground/10 to-transparent my-8" />
     </div>
