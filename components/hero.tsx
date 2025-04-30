e7559183c7c125a8383e287045bea39dbc2f4d51
Juan Bernal <mesieou@gmail.com>
2025-04-30 12:08:30 +1000

    made the waitlist form responsive

--- components/hero.tsx
+++ components/hero.tsx
@@ -7,8 +7,7 @@ import JoinWaitlist  from "@/components/waitlist-form";
 export default function Header() {
   return (
     <div className="flex flex-col gap-16 items-center">
-      <div className="flex gap-8 justify-center items-center">
-
+      <div className="flex flex-col-reverse md:flex-row gap-8 justify-center items-center">
         <div className="w-full sm:w-3/4 md:w-1/2 lg:w-1/3 h-auto rounded-lg shadow-lg">
           <Video
             src={botDemo}
@@ -20,7 +19,6 @@ export default function Header() {
           <p className="text-3xl lg:text-4xl !leading-tight mx-auto max-w-xl text-center">
             We help mobile business to manage their bookings and calendars with smart agents{" "}
           </p>
-          <p></p>
           <div className="mt-32">
             <JoinWaitlist />  
           </div> 
