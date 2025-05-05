c6f401e0fabc48c7880dabf183c2813f366f4d79
Lucas Hsueh <hsuehlucas@gmail.com>
2025-05-05 12:59:53 +1000

    papito vercel fixes + cursor fix

--- app/instruments/page.tsx
+++
@@ -1,9 +0,0 @@
-import { createClient } from '@/utils/supabase/server';
-import { data } from 'autoprefixer';
-
-export default async function Instruments() {
-  const supabase = await createClient();
-  const { data: instruments, error } = await supabase.from("instruments").select();
-  console.log({ instruments, error }); // 👈 add this
-  return <pre>{JSON.stringify(instruments, null, 2)}</pre>
-}
