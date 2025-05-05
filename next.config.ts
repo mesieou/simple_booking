037df60bd96bbf1160aa1c6c54d086d6f4677432
Juan Bernal <mesieou@gmail.com>
2025-05-05 18:07:43 +1000

    fixing bug

--- next.config.ts
+++ next.config.ts
@@ -3,10 +3,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  images: {
-    remotePatterns: [],
-    unoptimized: true
-  }
 };
 
 export default withNextVideo(nextConfig);
