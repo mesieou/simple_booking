98d8eed4b8e83fac7f39693104aa3c8823541714
Juan Bernal <mesieou@gmail.com>
2025-05-05 17:49:02 +1000

    trying to fix deployment

--- lib/supabase/admin.ts
+++ lib/supabase/admin.ts
@@ -1,12 +1,12 @@
 import { createServerClient } from "@supabase/ssr";
 import { cookies } from "next/headers";
 
-export const createClient = async () => {
+export const createAdminClient = async () => {
   const cookieStore = await cookies();
 
   return createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    process.env.SUPABASE_SERVICE_ROLE_KEY!,
     {
       cookies: {
         getAll() {
@@ -26,4 +26,4 @@ export const createClient = async () => {
       },
     }
   );
-};
+}; 
