2241dfa05d51b0a04c2abafe8805402a13fa31e6
Lucas Hsueh <hsuehlucas@gmail.com>
2025-07-05 20:17:26 +1000

    merge conflicts

--- jest.config.js
+++ jest.config.js
@@ -10,18 +10,33 @@ const customJestConfig = {
   testEnvironment: 'node',
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/$1'
+    '^@/(.*)$': '<rootDir>/$1',
+    '^@components/(.*)$': '<rootDir>/components/$1',
+    '^@app/(.*)$': '<rootDir>/app/$1',
+    '^@lib/(.*)$': '<rootDir>/lib/$1'
   },
   transform: {
     '^.+\\.tsx?$': ['ts-jest', {
-      useESM: true
+      useESM: true,
+      tsconfig: {
+        jsx: 'react-jsx'
+      }
     }]
   },
   transformIgnorePatterns: [
     'node_modules/(?!(got|cheerio|normalize-url)/)'
   ],
   extensionsToTreatAsEsm: ['.ts'],
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node']
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  testPathIgnorePatterns: [
+    '<rootDir>/.next/',
+    '<rootDir>/node_modules/'
+  ],
+  globals: {
+    'ts-jest': {
+      useESM: true
+    }
+  }
 };
 
 module.exports = createJestConfig(customJestConfig);
